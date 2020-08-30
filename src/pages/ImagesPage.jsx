b7abbb86730b276730635311a1c02c8692ab95f2
Aaditya Taparia <aadityataparia@gmail.com>
2020-08-30 22:28:49 +0900

    feat: add back to top button on home page

--- src/pages/ImagesPage.jsx
+++ src/pages/ImagesPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Page from "../components/Page";
 import ImagesList from "../components/ImagesList";
+import { BackTop } from "antd";
 
 const ImagesPage = ({ setFavorite }) => (
   <Page>
     <ImagesList setFavorite={setFavorite}></ImagesList>
+    <BackTop />
   </Page>
 );
 
