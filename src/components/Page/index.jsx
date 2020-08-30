74abcbe623be727e843e0c49096b4f291267dfd3
Aaditya Taparia <aadityataparia@gmail.com>
2020-08-30 22:43:06 +0900

    refactor: show back to top on all pages

--- src/components/Page/index.jsx
+++ src/components/Page/index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import { BackTop } from "antd";
 
 const MinDiv = styled.div`
   min-height: calc(100vh - 64px);
 `;
 
 const Page = ({ children }) => {
-  return <MinDiv>{children}</MinDiv>;
+  return (
+    <MinDiv>
+      {children}
+      <BackTop></BackTop>
+    </MinDiv>
+  );
 };
 
 export default Page;
--- src/pages/ImagesPage.jsx
+++ src/pages/ImagesPage.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Page from "../components/Page";
 import ImagesList from "../components/ImagesList";
-import { BackTop } from "antd";
 
 const ImagesPage = ({ setFavorite }) => (
   <Page>
     <ImagesList setFavorite={setFavorite}></ImagesList>
-    <BackTop />
   </Page>
 );
 
